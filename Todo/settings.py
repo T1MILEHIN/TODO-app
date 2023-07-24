b90291cdf132f0c537ed821e12498e2996ab935f
Timi <125024606+T1MILEHIN@users.noreply.github.com>
2023-07-24 22:42:32 +0100

    Update settings.py

--- Todo/settings.py
+++ Todo/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_URL = 'user:login'
+LOGIN_URL = '/USER/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
